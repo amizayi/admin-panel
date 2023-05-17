4d9f99221738d0de7e621559a3ef20e2df72a27d
DevAmiza <dev.amirrezaa@gmail.com>
2023-05-17 17:15:04 +0330

    add user seeder

---
+++ Modules/User/Database/Seeders/UserTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\User\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+use Modules\User\Entities\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        User::create([
+           'username' => "DevAmiza",
+           'first_name' => "Amirreza",
+           'last_name' => "Rezaei",
+           'full_name' => "Amirreza Rezaei",
+           'email' => "admin@admin.com",
+           'email_verified_at' => now(),
+           'password' => Hash::make("adminadmin"),
+        ]);
+    }
+}
