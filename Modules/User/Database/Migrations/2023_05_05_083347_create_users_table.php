67211135acbadd297e6526126be4b343b4bd5deb
DevAmiza <dev.amirrezaa@gmail.com>
2023-05-17 16:36:40 +0330

    change users fields

--- Modules/User/Database/Migrations/2023_05_05_083347_create_users_table.php
+++ Modules/User/Database/Migrations/2023_05_05_083347_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -15,7 +16,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('full_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
