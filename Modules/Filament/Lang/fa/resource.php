e4a434cd5a831af96c73e78e9cd5ee17fa28fb8a
DevAmiza <dev.amirrezaa@gmail.com>
2023-05-17 17:14:09 +0330

    add lang for nav and resource to filament module

---
+++ Modules/Filament/Lang/fa/resource.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'user' => [
+        'icon' => 'heroicon-o-users',
+        'nav' => [
+            'sort' => '1',
+            'group' => 'users',
+        ],
+        'trans' => [
+            'single' => 'کاربر',
+            'plural' => 'کاربران',
+        ],
+    ],
+];
