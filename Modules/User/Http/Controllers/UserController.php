3cb0cd318e2a7efde29463200a1120e65fd26aba
DevAmiza <RealDevAmiza@mail.com>
2023-05-31 18:30:57 +0330

    mk user api routes

---
+++ Modules/User/Http/Controllers/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\User\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class UserController extends Controller
+{
+
+}
--- Modules/User/Routes/api.php
+++ Modules/User/Routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+
+use Modules\User\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/$LOWER_NAME$', function (Request $request) {
-    return $request->user();
+Route::prefix('/user')->controller(UserController::class)->group(function () {
+    Route::get('/',                   'index');
+    Route::post('/store',             'store');
+    Route::put('/update',             'store');
+    Route::delete('/destroy',       'destroy');
+    Route::post('/multi-delete',     'delete');
 });
