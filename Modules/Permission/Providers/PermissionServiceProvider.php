75b8a0f403361d6a1e8ad5bdd955c0c00168fae1
MrAmiza <hjrezi1999@gmail.com>
2023-06-07 17:33:13 +0330

    permission module created

---
+++ Modules/Permission/Providers/PermissionServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Modules\Permission\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PermissionServiceProvider extends ServiceProvider
+{
+    /**
+     * @var string $moduleName
+     */
+    protected $moduleName = 'Permission';
+
+    /**
+     * @var string $moduleNameLower
+     */
+    protected $moduleNameLower = 'permission';
+
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
