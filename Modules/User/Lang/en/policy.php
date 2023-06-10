7600c995ddeae842ee4b200a432af539f209f3ff
MrAmiza <hjrezi1999@gmail.com>
2023-06-10 21:55:20 +0330

    add lang for title permissions

---
+++ Modules/User/Lang/en/policy.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'permission' => [
+        'user' => [
+            'parent'  => '👤 User Access Permissions',
+            'index'   => '👀 View Users',
+            'store'   => '✏️ Create User',
+            'update'  => '🖊️ Edit User Details',
+            'delete'  => '🗑️ Delete User',
+        ],
+    ]
+];
