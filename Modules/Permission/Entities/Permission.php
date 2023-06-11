2d1d19cd07ae8373058f8e4857eec8714464577e
MrAmiza <hjrezi1999@gmail.com>
2023-06-11 20:11:24 +0330

    use trait on Permission and Role Model

--- Modules/Permission/Entities/Permission.php
+++ Modules/Permission/Entities/Permission.php
@@ -2,9 +2,10 @@
 
 namespace Modules\Permission\Entities;
 
+use Modules\Permission\Traits\HasCustomRolePermission;
 use Spatie\Permission\Models\Permission as SpatiePermission;
 
 class Permission extends SpatiePermission
 {
-
+    use HasCustomRolePermission;
 }
--- Modules/Permission/Entities/Role.php
+++ Modules/Permission/Entities/Role.php
@@ -2,9 +2,10 @@
 
 namespace Modules\Permission\Entities;
 
+use Modules\Permission\Traits\HasCustomRolePermission;
 use Spatie\Permission\Models\Role as SpatieRole;
 
 class Role extends SpatieRole
 {
-
+    use HasCustomRolePermission;
 }
