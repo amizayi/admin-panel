020886243af27607688c5cb897c05fdea2dafd92
MrAmiza <hjrezi1999@gmail.com>
2023-06-12 19:57:40 +0330

    permission resource completed

---
+++ Modules/Permission/Transformers/Permission/PermissionResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Permission\Transformers\Permission;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Modules\Permission\Fields\PermissionFields;
+use Modules\Permission\Fields\RoleFields;
+
+class PermissionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'          => $this->{PermissionFields::ID},
+            'key'         => $this->{PermissionFields::NAME},
+            'title'       => $this->{PermissionFields::TITLE},
+            'parent_id'   => $this->{PermissionFields::PARENT_ID},
+            'parent_name' => $this->parent?->name,
+            'children'    => $this->children,
+        ];
+    }
+}
