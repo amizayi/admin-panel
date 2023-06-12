660b50b1442b535aa49cb985e3618334e244c331
MrAmiza <hjrezi1999@gmail.com>
2023-06-12 19:58:14 +0330

    permission controller completed

--- Modules/Permission/Http/Controllers/Api/PermissionController.php
+++ Modules/Permission/Http/Controllers/Api/PermissionController.php
@@ -2,11 +2,44 @@
 
 namespace Modules\Permission\Http\Controllers\Api;
 
-use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
+use Illuminate\Http\JsonResponse;
+use Modules\Permission\Entities\Permission;
+use Modules\Api\Http\Controllers\ApiController;
+use Modules\Permission\Fields\PermissionFields;
+use Modules\Permission\Transformers\Permission\PermissionResourceCollection;
+use Modules\Permission\Transformers\Permission\PermissionResource;
 
-class PermissionController extends Controller
+class PermissionController extends ApiController
 {
+    /**
+     * get list of Permission
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $permissions = Permission::with('children')
+            ->select([
+                PermissionFields::ID,
+                PermissionFields::NAME,
+                PermissionFields::TITLE
+            ])
+            ->whereNull(PermissionFields::PARENT_ID)
+            ->get();
+
+        return $this->successResponse(new PermissionResourceCollection($permissions), __response());
+    }
+
+
+    /**
+     * get Permission by id
+     *
+     * @param Permission $permission
+     * @return JsonResponse
+     */
+    public function show(Permission $permission): JsonResponse
+    {
+        return $this->successResponse(new PermissionResource($permission),__response());
+    }
 
 }
