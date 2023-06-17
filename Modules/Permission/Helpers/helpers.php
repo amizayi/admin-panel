3a81d8ddf1f2d0b74e9bac862629f2a5f846fadc
MrAmiza <hjrezi1999@gmail.com>
2023-06-17 10:53:15 +0330

    register role repo

--- Modules/Permission/Helpers/helpers.php
+++ Modules/Permission/Helpers/helpers.php
@@ -1,15 +1,28 @@
 <?php
 
 use Modules\Permission\Repositories\Contracts\PermissionRepository;
+use Modules\Permission\Repositories\Contracts\RoleRepository;
 
 if (!function_exists('permission')) {
     /**
-     * Get the user repo.
+     * Get the Permission repo.
      *
      * @return PermissionRepository
      */
     function permission(): PermissionRepository
     {
         return resolve(PermissionRepository::class);
+    }
+}
+
+if (!function_exists('role')) {
+    /**
+     * Get the Role repo.
+     *
+     * @return RoleRepository
+     */
+    function role(): RoleRepository
+    {
+        return resolve(RoleRepository::class);
     }
 }
