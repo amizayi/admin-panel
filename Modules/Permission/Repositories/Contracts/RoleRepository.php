0a14f838621b3061c060ca8f73a8491d871307fd
MrAmiza <hjrezi1999@gmail.com>
2023-06-19 15:33:36 +0330

    add method to roleRepo deleteRoleAndChildren

--- Modules/Permission/Repositories/Contracts/RoleRepository.php
+++ Modules/Permission/Repositories/Contracts/RoleRepository.php
@@ -2,8 +2,17 @@
 
 namespace Modules\Permission\Repositories\Contracts;
 
+
 use Prettus\Repository\Contracts\RepositoryInterface;
 
 interface RoleRepository extends RepositoryInterface
 {
+    /**
+     * Delete a role and all its children including their permissions.
+     *
+     * @param int $id The ID of the role to delete.
+     * @return  mixed deleted role object.
+     */
+    public function deleteRoleAndChildren(int $id): mixed;
+
 }
