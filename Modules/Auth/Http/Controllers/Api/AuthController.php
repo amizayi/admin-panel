a8413fbe53673a0fb8b0032912b7bb35377cbdc6
MrAmiza <hjrezi1999@gmail.com>
2023-06-22 08:17:21 +0330

    mk auth and otp controller

---
+++ Modules/Auth/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\Auth\Http\Controllers\Api;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class AuthController extends Controller
+{
+
+}
