c033d7f4ceb4be25d0f7c14ce4b034b2e7bccfaf
MrAmiza <hjrezi1999@gmail.com>
2023-06-23 17:14:20 +0330

    remove auth lang from api module

--- Modules/Api/Lang/en/response.php
+++ Modules/Api/Lang/en/response.php
@@ -2,23 +2,15 @@
 
 return [
     'success' => [
-        'base'          => '✅ Successfully',
-        'store'         => '✨ :name created',
-        'update'        => '💡 :name updated',
-        'destroy'       => '🗑️ :name deleted',
+        'base'    => '✅ Successfully',
+        'store'   => '✨ :name created',
+        'update'  => '💡 :name updated',
+        'destroy' => '🗑️ :name deleted',
     ],
-    'error'   => [
-        'base'          => '❌ Unsuccessful',
-        'store'         => '🚫 Unable to create :name',
-        'update'        => '🛑 Unable to update :name',
-        'destroy'       => '🚯 Unable to delete :name',
-    ],
-    'auth'    => [
-        'login'         => '🔐 Login successful',
-        'logout'        => '👋 Logout successful',
-        'unauthorized'  => '🚫 Unauthorized access',
-        'failed'        => '❌ Authentication failed',
-        'throttle'      => '⏳ Too many login attempts. Please try again later.',
-        'incorrect_pwd' => '🔑 Incorrect password. Please try again.',
-    ],
+    'error' => [
+        'base'    => '❌ Unsuccessful',
+        'store'   => '🚫 Unable to create :name',
+        'update'  => '🛑 Unable to update :name',
+        'destroy' => '🚯 Unable to delete :name',
+    ]
 ];
