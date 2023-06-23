141cf0cdb7128874de69be83e692abcaa1631a5b
MrAmiza <hjrezi1999@gmail.com>
2023-06-23 16:42:43 +0330

    add auth response on lang

--- Modules/Api/Lang/en/response.php
+++ Modules/Api/Lang/en/response.php
@@ -2,15 +2,23 @@
 
 return [
     'success' => [
-        'base'    => '✅ Successfully',
-        'store'   => '✨ :name created',
-        'update'  => '💡 :name updated',
-        'destroy' => '🗑️ :name deleted',
+        'base'          => '✅ Successfully',
+        'store'         => '✨ :name created',
+        'update'        => '💡 :name updated',
+        'destroy'       => '🗑️ :name deleted',
     ],
     'error'   => [
-        'base'    => '❌ Unsuccessful',
-        'store'   => '🚫 Unable to create :name',
-        'update'  => '🛑 Unable to update :name',
-        'destroy' => '🚯 Unable to delete :name',
+        'base'          => '❌ Unsuccessful',
+        'store'         => '🚫 Unable to create :name',
+        'update'        => '🛑 Unable to update :name',
+        'destroy'       => '🚯 Unable to delete :name',
+    ],
+    'auth'    => [
+        'login'         => '🔐 Login successful',
+        'logout'        => '👋 Logout successful',
+        'unauthorized'  => '🚫 Unauthorized access',
+        'failed'        => '❌ Authentication failed',
+        'throttle'      => '⏳ Too many login attempts. Please try again later.',
+        'incorrect_pwd' => '🔑 Incorrect password. Please try again.',
     ],
 ];
