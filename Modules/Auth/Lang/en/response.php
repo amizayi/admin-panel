f63253720ac2ffc4c2e9ec3de2f5298fe7d5c0e1
MrAmiza <hjrezi1999@gmail.com>
2023-06-23 17:14:33 +0330

    add auth lang on auth module

---
+++ Modules/Auth/Lang/en/response.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'login'           => '🔐 Login successful',
+    'logout'          => '👋 Logout successful',
+    'unauthorized'    => '🚫 Unauthorized access',
+    'failed'          => '❌ Authentication failed',
+    'throttle'        => '⏳ Too many login attempts. Please try again later.',
+    'incorrect_pwd'   => '🔑 Incorrect password. Please try again.',
+    'user_not_exists' => '👤 User does not exist. Please register first.',
+    'match_error'     => '⚠️ Username and password do not match.',
+];
