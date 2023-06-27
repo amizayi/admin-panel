d5bc2daa206093363adfcac52e333016ff12851e
MrAmiza <hjrezi1999@gmail.com>
2023-06-27 09:14:08 +0330

    add const otp fields

---
+++ Modules/Auth/Fields/OtpFields.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Auth\Fields;
+
+/*
+|--------------------------------------------------------------------------
+| Model Const FIELDS
+|--------------------------------------------------------------------------
+*/
+
+class OtpFields
+{
+    const USERID = 'id';
+    const CODE   = 'code';
+    const TOKEN  = 'token';
+    const MOBILE = 'mobile';
+    const EMAIL  = 'email';
+}
