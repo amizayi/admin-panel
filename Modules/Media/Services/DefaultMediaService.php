397b112ce5e2f165cebeb05e37d51506f6b175fc
MrAmiza <hjrezi1999@gmail.com>
2023-07-03 11:32:15 +0330

    get details of media in service

--- Modules/Media/Services/DefaultMediaService.php
+++ Modules/Media/Services/DefaultMediaService.php
@@ -3,32 +3,46 @@
 namespace Modules\Media\Services;
 
 use Carbon\Carbon;
+use Illuminate\Http\UploadedFile;
 
 class DefaultMediaService
 {
     /**
-     * Generate a filename with the current date and time.
+     * Generate a full path file with the current date and time.
      *
      * @return string
      */
-    public function generateFilename(): string
+    public function generateFullPath(): string
     {
-        $dateTime   = Carbon::now();
-        $folderName = $this->generateDirectoryPath($dateTime);
+        $dateTime = Carbon::now();
+        $dirPath  = $dateTime->format('Y/m/d/H');;
+        $fileName = $dateTime->format('YmdHisu');
 
-        return sprintf('%s/%s.jpg', $folderName, $dateTime->format('YmdHisu'));
+        return sprintf('%s/%s.jpg', $dirPath, $fileName);
     }
 
     /**
-     * Generate a directory path based on the provided or current date and time.
+     * Get details of the uploaded file.
      *
-     * @param Carbon|null $dateTime
-     * @return string
+     * @param UploadedFile $file
+     * @return array
      */
-    public function generateDirectoryPath(?Carbon $dateTime): string
+    public function fileDetails(UploadedFile $file): array
     {
-        $dateTime = $dateTime ?? Carbon::now();
+        $generatePath = $this->generateFullPath();
+        $originalName = $file->getClientOriginalName();
+        $mimeType     = $file->getMimeType();
+        $extension    = $file->getClientOriginalExtension();
+        $size         = $file->getSize();
+        $creatorId    = auth()->user()->id ?? null;
 
-        return $dateTime->format('Y/m/d/H');
+        return [
+            'file_name'     => getFileNameFromPath($generatePath), // get fileName from helper
+            'original_name' => $originalName,
+            'extension'     => $extension,
+            'mimetype'      => $mimeType,
+            'size'          => $size,
+            'creator_id'    => $creatorId,
+        ];
     }
 }
