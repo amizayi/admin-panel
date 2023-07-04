dc05857da99e4ba318ec1418b34f684365886af7
MrAmiza <hjrezi1999@gmail.com>
2023-07-04 07:50:29 +0330

    add media fields

---
+++ Modules/Media/Fields/MediaFields.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\Media\Fields;
+
+class MediaFields
+{
+    const FILE_NAME     = 'file_name';
+    const ORIGINAL_NAME = 'original_name';
+    const EXTENSION     = 'extension';
+    const MIMETYPE      = 'mimetype';
+    const SIZE          = 'size';
+    const DISK          = 'disk';
+    const CREATOR_ID    = 'creator_id';
+}
