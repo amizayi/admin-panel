a7fa0e842cd29ae686f71496afc766ef143eb1ab
MrAmiza <hjrezi1999@gmail.com>
2023-07-04 09:13:47 +0330

    add const fields to MediaFields

--- Modules/Media/Fields/MediaFields.php
+++ Modules/Media/Fields/MediaFields.php
@@ -4,7 +4,10 @@ namespace Modules\Media\Fields;
 
 class MediaFields
 {
+    const GENERATE_NAME = 'generate_name';
     const FILE_NAME     = 'file_name';
+    const PATH          = 'path';
+    const FULL_PATH     = 'full_path';
     const ORIGINAL_NAME = 'original_name';
     const EXTENSION     = 'extension';
     const MIMETYPE      = 'mimetype';
