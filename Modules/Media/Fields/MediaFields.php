31160b0db5e54a6a223c892641e512c39be8f057
MrAmiza <hjrezi1999@gmail.com>
2023-07-06 07:41:07 +0330

    mk media files table

--- Modules/Media/Fields/MediaFields.php
+++ Modules/Media/Fields/MediaFields.php
@@ -4,14 +4,12 @@ namespace Modules\Media\Fields;
 
 class MediaFields
 {
-    const GENERATE_NAME = 'generate_name';
     const FILE_NAME     = 'file_name';
-    const PATH          = 'path';
-    const FULL_PATH     = 'full_path';
     const ORIGINAL_NAME = 'original_name';
     const EXTENSION     = 'extension';
     const MIMETYPE      = 'mimetype';
     const SIZE          = 'size';
     const DISK          = 'disk';
     const CREATOR_ID    = 'creator_id';
+    const MORPH         = 'model';
 }
