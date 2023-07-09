3db7d355217a9b6fdb76a59d7ec5dbfd3e1fe5ef
MrAmiza <hjrezi1999@gmail.com>
2023-07-09 08:03:50 +0330

    create settings table

---
+++ Modules/Setting/Database/Migrations/2023_07_09_080215_create_settings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('key');
+            $table->string('title');
+            $table->text('description');
+            $table->text('value');
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};
