ec0bd761c195759f4c3ae5441deb8d235ced6e32
MrAmiza <hjrezi1999@gmail.com>
2023-07-24 12:20:17 +0330

    set default HttpStatusCode on processParseError

--- Modules/Api/Traits/ApiResponseTrait.php
+++ Modules/Api/Traits/ApiResponseTrait.php
@@ -6,9 +6,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\JsonResponse;
 use Exception;
 use Throwable;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Validation\ValidationException;
 
 
 trait ApiResponseTrait
@@ -52,9 +49,10 @@ trait ApiResponseTrait
      *
      * @param $getMessage
      * @param Exception|Throwable $exception
+     * @param int $statusCode
      * @return JsonResponse
      */
-    protected function processParseError($getMessage, Exception|Throwable $exception): JsonResponse
+    protected function processParseError($getMessage, Exception|Throwable $exception, int $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR): JsonResponse
     {
         $detail = [
             'file' => $exception->getFile(),
@@ -64,47 +62,13 @@ trait ApiResponseTrait
 
         // Log the error message and stack trace
         if(env('APP_DEBUG'))
-            logger()->error("⚠️ $getMessage", $detail);
+            logger()->error("[⚠️ $getMessage]", $detail);
 
         // Return a JSON response
         return response()->json([
             'status'  => false,
             'message' => $getMessage,
             'errors'  => $detail
-        ], Response::HTTP_BAD_REQUEST);
+        ], $statusCode);
     }
-
-
-//    /**
-//     * Handle a failed validation attempt.
-//     *
-//     * @param Validator $validator
-//     * @return void
-//     *
-//     * @throws ValidationException
-//     */
-//    protected function failedValidation(Validator $validator): void
-//    {
-//        throw new ValidationException(
-//            $validator,
-//            $this->failedValidationResponse((array)$validator->errors())
-//        );
-//    }
-
-
-//    /**
-//     * Send the response after a failed validation attempt.
-//     *
-//     * @param array $errors
-//     * @return JsonResponse
-//     */
-//    protected function failedValidationResponse(array $errors): JsonResponse
-//    {
-//        return response()->json([
-//            'status'  => false,
-//            'message' => 'The given data was invalid.',
-//            'errors'  => $errors,
-//        ], Response::HTTP_UNPROCESSABLE_ENTITY);
-//    }
-
 }
