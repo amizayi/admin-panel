39a12f1315349e7e9a47bc5d372b48034092fa1f
MrAmiza <hjrezi1999@gmail.com>
2023-07-27 20:43:02 +0330

    log activit module add

---
+++ Modules/LogActivity/Providers/LogActivityServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Modules\LogActivity\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
+
+class LogActivityServiceProvider extends ServiceProvider
+{
+    /**
+     * @var string $moduleName
+     */
+    protected $moduleName = 'LogActivity';
+
+    /**
+     * @var string $moduleNameLower
+     */
+    protected $moduleNameLower = 'logactivity';
+
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
