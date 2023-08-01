63214aa255910d585f9b9fa239dd4917cdc6c871
MrAmiza <hjrezi1999@gmail.com>
2023-08-01 10:14:24 +0330

    change logger make method name

--- Modules/LogActivity/Services/Logger.php
+++ Modules/LogActivity/Services/Logger.php
@@ -5,19 +5,18 @@ namespace Modules\LogActivity\Services;
 
 use Jenssegers\Agent\Agent;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Modules\LogActivity\Fields\LogFields;
 
 class Logger
 {
-    public static function make(Request $request): void
+    public static function info(Request $request): array
     {
         $agent = new Agent();
 
-        $logInfo = [
+        return [
             LogFields::IP_ADDRESS   => $request->ip(),
             LogFields::URL          => $request->url(),
-            LogFields::ACTION       => $request->route()->getActionMethod(),
+            LogFields::ACTION       => $request->route()?->getActionMethod(),
             LogFields::USER_ID      => auth()->user()?->id ?? null,
             LogFields::DEVICE       => $agent->device(),
             LogFields::PLATFORM     => $agent->platform(),
@@ -26,8 +25,5 @@ class Logger
             LogFields::IS_DESKTOP   => $agent->isDesktop(),
             LogFields::IS_TABLET    => $agent->isTablet(),
         ];
-
-
-        Log::info(json_encode($logInfo, JSON_PRETTY_PRINT));
     }
 }
