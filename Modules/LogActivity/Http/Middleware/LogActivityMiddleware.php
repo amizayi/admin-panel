a7e0f34802014c078600cd07c3d24897541fd12b
MrAmiza <hjrezi1999@gmail.com>
2023-08-01 10:15:17 +0330

    refactor log activity middleware

--- Modules/LogActivity/Http/Middleware/LogActivityMiddleware.php
+++ Modules/LogActivity/Http/Middleware/LogActivityMiddleware.php
@@ -3,7 +3,10 @@
 namespace Modules\LogActivity\Http\Middleware;
 
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Modules\LogActivity\Jobs\LoggerJob;
 use Modules\LogActivity\Services\Logger;
 
 class LogActivityMiddleware
@@ -17,7 +20,14 @@ class LogActivityMiddleware
      */
     public function handle(Request $request, Closure $next): mixed
     {
-        Logger::make($request);
+        try {
+            $logInfo = Logger::info($request);
+            LoggerJob::dispatch($logInfo)->onQueue('activity_logs'); // Optionally, you can specify the queue name.
+        } catch (Exception $e) {
+            // Log the exception for debugging purposes
+            Log::error('Error in Logger middleware: ' . $e->getMessage());
+        }
+
         return $next($request);
     }
 }
