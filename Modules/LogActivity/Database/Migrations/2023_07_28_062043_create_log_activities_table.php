df57c1536251d3248ce4f0c87ce6059b78add019
MrAmiza <hjrezi1999@gmail.com>
2023-08-03 09:00:24 +0330

    refactor log activities table

--- Modules/LogActivity/Database/Migrations/2023_07_28_062043_create_log_activities_table.php
+++ Modules/LogActivity/Database/Migrations/2023_07_28_062043_create_log_activities_table.php
@@ -15,21 +15,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('log_activities', function (Blueprint $table) {
+            $table->id();
+            $table->string(LogFields::IP_ADDRESS);
+            $table->string(LogFields::URL);
+            $table->string(LogFields::ACTION);
             $table->unsignedBigInteger(LogFields::USER_ID);
-            $table->string(LogFields::ACTIVITY);
-            $table->text(LogFields::DESCRIPTION)->nullable();
-            $table->string(LogFields::IP_ADDRESS)->nullable();
-            $table->text(LogFields::USER_AGENT)->nullable();
-            $table->string(LogFields::DEVICE)->nullable();
-            $table->string(LogFields::PLATFORM)->nullable();
-            $table->string(LogFields::BROWSER)->nullable();
-            $table->string(LogFields::BROWSER_VERSION)->nullable();
-            $table->boolean(LogFields::IS_MOBILE)->default(false);
-            $table->boolean(LogFields::IS_DESKTOP)->default(false);
-            $table->boolean(LogFields::IS_TABLET)->default(false);
-            $table->text(LogFields::LANGUAGES)->nullable();
-            $table->boolean(LogFields::IS_ROBOT)->default(false);
-            $table->string(LogFields::ROBOT_NAME)->nullable();
+            $table->string(LogFields::DEVICE);
+            $table->string(LogFields::PLATFORM);
+            $table->string(LogFields::BROWSER);
+            $table->boolean(LogFields::IS_MOBILE);
+            $table->boolean(LogFields::IS_DESKTOP);
+            $table->boolean(LogFields::IS_TABLET);
+            $table->dateTime(LogFields::DATE_TIME);
             $table->timestamps();
         });
     }
