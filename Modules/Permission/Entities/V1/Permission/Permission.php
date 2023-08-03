868b49e87c20817b1fcc5029fba6eb7a5ffbaae0
MrAmiza <hjrezi1999@gmail.com>
2023-08-03 17:35:51 +0330

    remove old used on model

--- Modules/Permission/Entities/V1/Permission/Permission.php
+++ Modules/Permission/Entities/V1/Permission/Permission.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Permission\Entities\V1\Permission;
 
-use Modules\Permission\Traits\HasCustomRolePermission;
 use Spatie\Permission\Models\Permission as SpatiePermission;
 
 class Permission extends SpatiePermission
--- Modules/Permission/Entities/V1/Role/Role.php
+++ Modules/Permission/Entities/V1/Role/Role.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Permission\Entities\V1\Role;
 
-use Modules\Permission\Traits\HasCustomRolePermission;
 use Spatie\Permission\Models\Role as SpatieRole;
 
 class Role extends SpatieRole
