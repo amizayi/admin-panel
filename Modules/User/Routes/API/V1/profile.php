cae614e7d720e80e8900cde17ec20f80e7d742bd
MrAmiza <hjrezi1999@gmail.com>
2023-08-04 11:06:39 +0330

    set auth:sanctum middleware for profile info route

--- Modules/User/Routes/API/V1/profile.php
+++ Modules/User/Routes/API/V1/profile.php
@@ -1,11 +1,13 @@
 <?php
 
 
+use Modules\User\Http\Controllers\Api\V1\ProfileController;
+
 Route::prefix('v1/profile')
 
     ->as('api.v1.profile.')
 
-    ->middleware('log.activity')
+    ->middleware(['log.activity','auth:sanctum'])
 
     ->controller(ProfileController::class)
 
