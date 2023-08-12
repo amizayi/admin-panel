8471d3befbb545d3ccf88ce39074e5aa3bd6e20f
MrAmiza <hjrezi1999@gmail.com>
2023-08-12 10:27:26 +0330

    add consts files on kernel module

---
+++ Modules/Kernel/Constants/V1/Tables.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Kernel\Constants\V1;
+
+class Tables
+{
+    const USERS                 = 'users';
+    const LOG_ACTIVITIES        = 'log_activities';
+    const MEDIA_FILE            = 'media_files';
+    const SETTINGS              = 'settings';
+    const PERMISSIONS           = 'permissions';
+    const ROLES                 = 'roles';
+    const MODEL_HAS_ROLES       = 'model_has_roles';
+    const MODEL_HAS_PERMISSIONS = 'model_has_permissions';
+    const ROLE_HAS_PERMISSION   = 'role_has_permissions';
+}
